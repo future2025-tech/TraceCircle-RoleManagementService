0e50368a025f9c119c3222b146f675ea3da95322
SHAIK AMEER <shaikameerjann@gmail.com>
2025-11-13 11:51:22 +0530

    Adding Cors for Integrating with Front-End

--- RolesManagementService/src/main/java/com/TraceCircle/RolesManagementService/Controller/AuthController.java
+++ RolesManagementService/src/main/java/com/TraceCircle/RolesManagementService/Controller/AuthController.java
@@ -21,7 +21,6 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
 
     private final AuthManagementService authService;
